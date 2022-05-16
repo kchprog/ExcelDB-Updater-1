8ae7436a7994e90b69d37bd60abfba130e8731bd
Kevin Chen <86860267+kchprog@users.noreply.github.com>
2022-05-16 12:49:24 -0400

    Update Excel read-write test.py

--- Excel read-write test.py
+++ Excel read-write test.py
@@ -7,6 +7,7 @@
     and the application will not overwrite the data manually added by the user
 """
 
+from email import contentmanager
 from functools import total_ordering
 from openpyxl import Workbook
 from openpyxl import load_workbook
@@ -19,6 +20,8 @@ from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files.file import File
 
+import os
+
 '''
 Approach: Use the Excel API to directly access and manipulate the data on the online spreadsheet.
 '''
@@ -27,7 +30,7 @@ def main():
     url = 'https://cmhcschl-my.sharepoint.com/personal/kchen_cmhc-schl_gc_ca'
     relative_url = '/personal/kchen_cmhc-schl_gc_ca/Documents/Database%20Revamp%20Project%20-%20API%20Test%20Storage.xlsx'
 
-    username = 'LOGIN' # Replace credentials with the tool's own credentials
+    username = 'USERNAME'
     password = 'PASSWORD'
 
 
@@ -38,15 +41,46 @@ def main():
         ctx.load(web)
         ctx.execute_query()
         print("Authentication successful")
-
-    response = File.open_binary(ctx, url)
-
+    else:
+        print("Authentication failed")
     # Test: Download file, open, edit, and reupload
-
-    filename = 'Database Revamp Project - API Test Storage.xlsx'
-
     # Download file
 
+    filename = 'Database Revamp Project - API Test Storage.xlsx'
     with open(filename, 'wb') as output_file:
         response = File.open_binary(ctx, relative_url)
         output_file.write(response.content)
+        
+    # Open file
+    wb = load_workbook(filename)
+    print(wb.get_named_ranges())
+    
+    ws = wb.active
+    
+    print(ws['A1'].value)
+    
+    ws['B1'] = 'Hello World'
+    
+    import datetime
+    
+    ws['B2'] = datetime.datetime.now()
+    
+    print(ws['B1'].value)
+    print(ws['B2'].value)
+    
+    
+    wb.save(filename)
+    
+    # attempt to upload file back to the sharepoint
+    
+    with open(filename, 'rb') as input_file:
+        file_content = input_file.read()
+    
+    target_folder = web.get_folder_by_server_relative_url("/personal/kchen_cmhc-schl_gc_ca/Documents/")
+    name = os.path.basename(filename)
+    target_file = target_folder.upload_file(name, file_content).execute_query()
+    
+    print("File has been uploaded to url: {0}".format(target_file.serverRelativeUrl))
+    
+if __name__ == '__main__':
+    main()
